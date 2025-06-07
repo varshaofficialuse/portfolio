2cb3b3ac54133dd94fabada3011774d555693e9b
varshaofficialuse <varshaofficialuse@gmail.com>
2025-06-07 16:59:03 +0530

    portfolio added

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// --------typing animation........
+
+var typed = new Typed(".typing",{
+   strings :["","React Js Developer","Python Developer"],
+   typeSpeed:100,
+   BackSpeed:60,
+   loop:true
+});
+
+
+// -------fireBase Setup
+
